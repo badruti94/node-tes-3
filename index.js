0aa5b6f790c6b96b1a2f39ce8ee5822b94937d78
Badru <badruakfm@gmail.com>
2022-02-26 16:07:24 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 const cors = require('cors')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(cors())
 
 app.get('/', (req, res) => {
