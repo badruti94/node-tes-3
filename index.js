958a1575a7d31f1ca0baf0c4a951a2224198f9f1
Badru <badruakfm@gmail.com>
2022-02-26 16:21:00 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
+const cloudinary = require('cloudinary').v2
 require('dotenv').config()
 const cors = require('cors')
-const cloudinary = require('cloudinary').v2
 
 const app = express()
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(cors())
 
 
 const storage = multer.diskStorage({
@@ -21,9 +16,6 @@ const storage = multer.diskStorage({
         cb(null, `${new Date().getTime()}-${file.originalname}`)
     }
 })
-const upload = multer({
-    storage
-}).single('file')
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -31,11 +23,17 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(cors())
+const upload = multer({
+    storage
+}).single('file')
 
 app.get('/', (req, res) => {
-    res.send('tes-9')
+    res.send('tes-6')
 })
-
 app.post('/tes', upload, async (req, res) => {
     const result = await cloudinary.uploader.upload(req.file.path, {
         folder: 'uploads',
@@ -46,7 +44,7 @@ app.post('/tes', upload, async (req, res) => {
     res.send('tes');
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT | 3000
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
+    console.log(`Running....`);
 })
