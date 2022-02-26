e622eb411e92e99e0b3272ec9b858dcddb5d8ede
Badru <badruakfm@gmail.com>
2022-02-26 16:38:41 +0700

    update

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/tes', upload, async (req, res) => {
     res.send('tes');
 })
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Running....`);
 })
